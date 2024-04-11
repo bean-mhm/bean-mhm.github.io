d51f7a6e54012e55d5c81c9042658a8f918fdaee
bean <harry.bean.dev@gmail.com>
2024-04-11 20:45:55 +0000

    updated articles and assets (WIP, still invisible in index)

---
+++ src/assets/code/cpu-triangle.py
@@ -0,0 +1,211 @@
+from PIL import Image
+
+
+WIDTH: int = 320
+HEIGHT: int = 240
+N_PIXELS: int = WIDTH * HEIGHT
+PNG_SAVE_PATH: str = 'C:/Users/bean/Desktop/cpu-triangle.png'
+
+
+class Vec2:
+    x: float
+    y: float
+
+    def __init__(self, x: float, y: float) -> None:
+        self.x = x
+        self.y = y
+
+    @staticmethod
+    def scalar(s: float):
+        return Vec2(s, s)
+
+    def __add__(self, other):
+        return Vec2(self.x + other.x, self.y + other.y)
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+
+    def __sub__(self, other):
+        return Vec2(self.x - other.x, self.y - other.y)
+
+    def __isub__(self, other):
+        self.x -= other.x
+        self.y -= other.y
+
+    def __mul__(self, other):
+        return Vec2(self.x * other.x, self.y * other.y)
+
+    def __imul__(self, other):
+        self.x *= other.x
+        self.y *= other.y
+
+    def __truediv__(self, other):
+        return Vec2(self.x / other.x, self.y / other.y)
+
+    def __idiv__(self, other):
+        self.x /= other.x
+        self.y /= other.y
+
+    def __neg__(self):
+        return Vec2(-self.x, -self.y)
+
+
+class Vec3:
+    x: float
+    y: float
+    z: float
+
+    def __init__(self, x: float, y: float, z: float) -> None:
+        self.x = x
+        self.y = y
+        self.z = z
+
+    @staticmethod
+    def scalar(s: float):
+        return Vec3(s, s, s)
+
+    def __add__(self, other):
+        return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        self.z += other.z
+
+    def __sub__(self, other):
+        return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __isub__(self, other):
+        self.x -= other.x
+        self.y -= other.y
+        self.z -= other.z
+
+    def __mul__(self, other):
+        return Vec3(self.x * other.x, self.y * other.y, self.z * other.z)
+
+    def __imul__(self, other):
+        self.x *= other.x
+        self.y *= other.y
+        self.z *= other.z
+
+    def __truediv__(self, other):
+        return Vec3(self.x / other.x, self.y / other.y, self.z / other.z)
+
+    def __idiv__(self, other):
+        self.x /= other.x
+        self.y /= other.y
+        self.z /= other.z
+
+    def __neg__(self):
+        return Vec3(-self.x, -self.y, -self.z)
+
+
+class Triangle:
+    v0: Vec2
+    v1: Vec2
+    v2: Vec2
+    v0_col: Vec3
+    v1_col: Vec3
+    v2_col: Vec3
+
+    def __init__(
+        self,
+        v0: Vec2,
+        v1: Vec2,
+        v2: Vec2,
+        v0_col: Vec3,
+        v1_col: Vec3,
+        v2_col: Vec3
+    ) -> None:
+        self.v0 = v0
+        self.v1 = v1
+        self.v2 = v2
+        self.v0_col = v0_col
+        self.v1_col = v1_col
+        self.v2_col = v2_col
+
+    def cart_to_bary(self, p: Vec2) -> Vec3:
+        return cart_to_bary(p, self.v0, self.v1, self.v2)
+
+
+# |a| * |b| * sin(theta)
+def cross_2d(a: Vec2, b: Vec2) -> float:
+    return (a.x * b.y) - (a.y * b.x)
+
+
+# triangle vertices must be ordered counterclockwise
+def cart_to_bary(
+    p: Vec2,
+    v0: Vec2,
+    v1: Vec2,
+    v2: Vec2
+) -> Vec3:
+    b = Vec3(
+        cross_2d(v1 - p, v2 - p),
+        cross_2d(v2 - p, v0 - p),
+        cross_2d(v0 - p, v1 - p)
+    )
+    b /= Vec3.scalar(cross_2d(v1 - v0, v2 - v0))
+    return b
+
+
+def bary_is_outside(b: Vec3) -> bool:
+    return min(b.x, b.y, b.z) < 0
+
+
+def idx_to_icoord(idx: int, width: int) -> tuple[int, int]:
+    return (idx % width, idx // width)
+
+
+def icoord_to_idx(x: int, y: int, width: int) -> int:
+    return x + (y * width)
+
+
+# create buffer and initialize all colors to black
+buf: list[Vec3] = []
+for i in range(N_PIXELS):
+    buf.append(Vec3.scalar(0))
+
+# triangle list
+tris: list[Triangle] = []
+tris.append(Triangle(
+    v0=Vec2(256, 48),
+    v1=Vec2(160, 192),
+    v2=Vec2(64, 48),
+    v0_col=Vec3(1, .2, .1),
+    v1_col=Vec3(.2, 1, .1),
+    v2_col=Vec3(.1, .2, 1)
+))
+
+for y in range(HEIGHT):
+    for x in range(WIDTH):
+        p = Vec2(x + .5, y + .5)
+
+        col = Vec3(.1, .1, .1)
+        for tri in tris:
+            bary: Vec3 = tri.cart_to_bary(p)
+            if bary_is_outside(bary):
+                continue
+
+            col = (Vec3.scalar(bary.x) * tri.v0_col) \
+                + (Vec3.scalar(bary.y) * tri.v1_col) \
+                + (Vec3.scalar(bary.z) * tri.v2_col)
+
+        idx = icoord_to_idx(x, HEIGHT - y - 1, WIDTH)
+        buf[idx] = col
+
+# RGB8 buffer
+buf_rgb8: list[tuple[int, int, int]] = []
+for i in range(N_PIXELS):
+    col: Vec3 = buf[i]
+    buf_rgb8.append((
+        int(col.x * 255),
+        int(col.y * 255),
+        int(col.z * 255)
+    ))
+
+# export to PNG
+img: Image = Image.new('RGB', (WIDTH, HEIGHT))
+img.putdata(buf_rgb8)
+img.save(PNG_SAVE_PATH, 'PNG')
