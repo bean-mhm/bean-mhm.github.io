9e196df5d76e39d86cbf144c5c2b49c313179061
bean <harry.bean.dev@gmail.com>
2024-04-18 07:26:06 +0000

    updated articles

--- docs/assets/code/cpu-triangle.py
+++ docs/assets/code/cpu-triangle.py
@@ -1,9 +1,12 @@
+import math
 from PIL import Image
 
 
 WIDTH: int = 320
 HEIGHT: int = 240
 N_PIXELS: int = WIDTH * HEIGHT
+
+TEXTURE_PATH: str = 'C:/Users/bean/Desktop/test-texture.png'
 PNG_SAVE_PATH: str = 'C:/Users/bean/Desktop/cpu-triangle.png'
 
 
@@ -153,6 +156,92 @@ def icoord_to_idx(x: int, y: int, width: int) -> int:
     return x + (y * width)
 
 
+def mix(a: float, b: float, t: float) -> float:
+    return a + t * (b - a)
+
+
+def mix_vec2(a: Vec2, b: Vec2, t: float) -> Vec2:
+    return a + Vec2.scalar(t) * (b - a)
+
+
+def mix_vec3(a: Vec3, b: Vec3, t: float) -> Vec3:
+    return a + Vec3.scalar(t) * (b - a)
+
+
+def mix_bilinear(
+    bl: float,  # value at the bottom left
+    br: float,  # bottom right
+    tl: float,  # top left
+    tr: float,  # top right
+    t_horz: float,  # blend weight from left to right
+    t_vert: float  # blend weight from bottom to top
+) -> float:
+    return mix(
+        mix(bl, br, t_horz),
+        mix(tl, tr, t_horz),
+        t_vert
+    )
+
+
+def mix_vec2_bilinear(
+    bl: Vec2,  # value at the bottom left
+    br: Vec2,  # bottom right
+    tl: Vec2,  # top left
+    tr: Vec2,  # top right
+    t_horz: float,  # blend weight from left to right
+    t_vert: float  # blend weight from bottom to top
+) -> float:
+    return mix_vec2(
+        mix_vec2(bl, br, t_horz),
+        mix_vec2(tl, tr, t_horz),
+        t_vert
+    )
+
+
+def mix_vec3_bilinear(
+    bl: Vec3,  # value at the bottom left
+    br: Vec3,  # bottom right
+    tl: Vec3,  # top left
+    tr: Vec3,  # top right
+    t_horz: float,  # blend weight from left to right
+    t_vert: float  # blend weight from bottom to top
+) -> float:
+    return mix_vec3(
+        mix_vec3(bl, br, t_horz),
+        mix_vec3(tl, tr, t_horz),
+        t_vert
+    )
+
+
+def texel_fetch(
+    tex: Image,
+    tex_data: list[tuple[int, int, int]],
+    icoord: tuple[int, int]
+) -> Vec3:
+    if icoord[0] < 0:
+        icoord = (0, icoord[1])
+    if icoord[0] >= tex.width:
+        icoord = (tex.width - 1, icoord[1])
+
+    if icoord[1] < 0:
+        icoord = (icoord[0], 0)
+    if icoord[1] >= tex.height:
+        icoord = (icoord[0], tex.height - 1)
+
+    idx = icoord_to_idx(
+        icoord[0],
+        icoord[1],
+        tex.width
+    )
+
+    col_rgb8: tuple[int, int, int] = tex_data[idx]
+    return Vec3(
+        col_rgb8[0] / 255.,
+        col_rgb8[1] / 255.,
+        col_rgb8[2] / 255.
+    )
+
+
 # create buffer and initialize all colors to black
 buf: list[Vec3] = []
 for i in range(N_PIXELS):
@@ -195,6 +284,11 @@ tris.append(Triangle(
     )
 ))
 
+# load the texture
+tex = Image.open(TEXTURE_PATH).convert(mode='RGB')
+tex_data: list[tuple[int, int, int]] = list(tex.getdata())
+
+# render
 for y in range(HEIGHT):
     for x in range(WIDTH):
         p = Vec2(x + .5, y + .5)
@@ -205,20 +299,44 @@ for y in range(HEIGHT):
             if bary_is_outside(bary):
                 continue
 
-            interp_col = \
+            interp_col: Vec3 = \
                 (Vec3.scalar(bary.x) * tri.v_col[0]) \
                 + (Vec3.scalar(bary.y) * tri.v_col[1]) \
                 + (Vec3.scalar(bary.z) * tri.v_col[2])
 
-            interp_uv = \
+            interp_uv: Vec2 = \
                 (Vec2.scalar(bary.x) * tri.v_uv[0]) \
                 + (Vec2.scalar(bary.y) * tri.v_uv[1]) \
                 + (Vec2.scalar(bary.z) * tri.v_uv[2])
 
-            col = Vec3(
-                interp_uv.x,
-                interp_uv.y,
-                0
+            sample_point: Vec2 = interp_uv * Vec2(tex.width, tex.height)
+
+            icoord_bl = (
+                int(math.floor(sample_point.x - .5)),
+                int(math.floor(sample_point.y - .5))
+            )
+            icoord_br = (icoord_bl[0] + 1, icoord_bl[1] + 0)
+            icoord_tl = (icoord_bl[0] + 0, icoord_bl[1] + 1)
+            icoord_tr = (icoord_bl[0] + 1, icoord_bl[1] + 1)
+
+            col_bl: Vec3 = texel_fetch(tex, tex_data, icoord_bl)
+            col_br: Vec3 = texel_fetch(tex, tex_data, icoord_br)
+            col_tl: Vec3 = texel_fetch(tex, tex_data, icoord_tl)
+            col_tr: Vec3 = texel_fetch(tex, tex_data, icoord_tr)
+
+            bl_center = Vec2(
+                icoord_bl[0] + .5,
+                icoord_bl[1] + .5
+            )
+            bilinear_offs: Vec2 = sample_point - bl_center
+
+            col: Vec3 = mix_vec3_bilinear(
+                col_bl,
+                col_br,
+                col_tl,
+                col_tr,
+                t_horz=bilinear_offs.x,
+                t_vert=bilinear_offs.y
             )
 
         idx = icoord_to_idx(x, HEIGHT - y - 1, WIDTH)
